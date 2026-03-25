d2587cff61e5aecf5b29f372946de54ee5f4abfb
dpsshivam879-commits <dps.shivam879@gmail.com>
2026-03-25 17:34:10 +0530

    initial commit

---
+++ activity5.py
@@ -0,0 +1,6 @@
+number=int(input("enter a number"))
+print("the number provided is=",number)
+if number%2==0:
+    print("the number provided is even")
+else:
+    print("the number provided is odd")   
